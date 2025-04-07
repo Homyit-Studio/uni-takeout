68da8a2e3e44cfe1914d784307c8631dca351580
ZhangXuelong <3052774637@qq.com>
2025-04-07 21:23:43 +0800

    拼团+订单

--- src/store/token.js
+++ src/store/token.js
@@ -1,14 +1,11 @@
 // 封装的 token 操作方法
 export const useTokenStore = () => {
-    // 硬编码的 token
-    const hardcodedToken = 'a33ef9d6-33cd-4d88-bb3a-a6c55d04c5f9';
-
     const setToken = (token) => {
         uni.setStorageSync('token_key', token);
     };
 
     const getToken = () => {
-        return uni.getStorageSync('token_key') || '';
+        return uni.getStorageSync('token_key');
     };
 
     const removeToken = () => {
